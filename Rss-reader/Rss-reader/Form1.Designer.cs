97333265aa25dd31b0d726319ae297852298955d
lukasbrolin <lukas.brolin@live.se>
2020-10-21 11:18:18 +0200

    Updates

--- Rss-reader/Rss-reader/Form1.Designer.cs
+++ Rss-reader/Rss-reader/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace Rss_reader
+﻿using System.Windows.Forms;
+
+namespace Rss_reader
 {
     partial class Form1
     {
@@ -28,13 +30,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.clmEpisode = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmFrequency = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmCategory = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clmEpisode,
+            this.clmName,
+            this.clmFrequency,
+            this.clmCategory});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.MaximumSize = new System.Drawing.Size(600, 250);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridView1.Size = new System.Drawing.Size(600, 250);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // clmEpisode
+            // 
+            this.clmEpisode.HeaderText = "Avsnitt";
+            this.clmEpisode.Name = "clmEpisode";
+            this.clmEpisode.Width = 80;
+            // 
+            // clmName
+            // 
+            this.clmName.HeaderText = "Namn";
+            this.clmName.Name = "clmName";
+            this.clmName.Width = 200;
+            // 
+            // clmFrequency
+            // 
+            this.clmFrequency.HeaderText = "Frekvens";
+            this.clmFrequency.Name = "clmFrequency";
+            this.clmFrequency.Width = 160;
+            // 
+            // clmCategory
+            // 
+            this.clmCategory.HeaderText = "Kategori";
+            this.clmCategory.Name = "clmCategory";
+            this.clmCategory.Width = 160;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1064, 681);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmEpisode;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmFrequency;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmCategory;
     }
 }
 
