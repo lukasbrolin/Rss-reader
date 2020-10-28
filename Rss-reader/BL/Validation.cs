06ca51f22238c4ce8510d84f967e398bd0d603c1
marcusskarud <50720833+marcusskarud@users.noreply.github.com>
2020-10-28 09:11:16 +0100

    Create Validation

---
+++ Rss-reader/BL/Validation.cs
@@ -0,0 +1,133 @@
+﻿using DAL;
+using DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace BL
+{
+    public static class Validation
+    {
+        // Metoderna nedan utför en specifik validering.
+        public static bool TextIsFilled(TextBox textBox, string textType)
+        {
+            if (textBox.Text.Length <= 3)
+            {
+                MessageBox.Show(textType + " måste innehålla minst 3 tecken långt!");
+                return false;
+            }
+            return true;
+        }
+
+        public static bool CategoryIsUnique(TextBox categoryTextBox, CategoryRepository categoryRepository)
+        {
+            foreach (var category in categoryRepository.GetAll())
+            {
+                if (categoryTextBox.Text == category.Title)
+                {
+                    MessageBox.Show("Den angivna kategorin finns redan!");
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool UrlIsUnique(TextBox urlTextBox, PodcastRepository podcastRepository)
+        {
+            foreach (var podcast in podcastRepository.GetAll())
+            {
+                if (urlTextBox.Text == podcast.Url)
+                {
+                    MessageBox.Show("Den angivna URL:en finns redan!");
+                    return false; }
+            }
+            return true;
+        }
+
+        public static bool UrlIsValid(TextBox urlTextBox)
+        {
+            if (UrlManager.GetTotalEpisodes(urlTextBox.Text) < 1)
+            {
+                MessageBox.Show("Den angivna URL:en är ogiltig!");
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool NameIsUnique(TextBox name, PodcastRepository podcastRepository)
+        {
+            foreach (var podcast in podcastRepository.GetAll())
+            {
+                if (podcast.Name == name.Text)
+                {
+                    MessageBox.Show("En podcast med detta namn finns redan tillagd!");
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool comboBoxItemSelected(ComboBox comboBox)
+        {
+            if (comboBox.SelectedItem == null)
+            {
+                MessageBox.Show("Ett värde måste väljas!");
+                return false;
+            }
+            return true;
+        }
+
+
+
+
+
+
+
+        // Metoderna nedan grupperar valideringsmetoderna ovan i metoder som utför flera valideringar på en gång.  
+        public static bool ValidateNewName(TextBox name, PodcastRepository podcastRepository)
+        {
+            if (TextIsFilled(name, "Namnfältet"))
+            {
+                if (NameIsUnique(name, podcastRepository))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool ValidateNewUrl(TextBox url, PodcastRepository podcastRepository)
+        {
+            if (TextIsFilled(url, "URL:en"))
+            {
+                if (UrlIsUnique(url, podcastRepository))
+                {
+                    if (UrlIsValid(url))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public static bool ValidateNewOrChangedCategory(TextBox name, CategoryRepository categoryRepository)
+        {
+            if (TextIsFilled(name, "Kategorin"))
+            {
+                if(CategoryIsUnique(name, categoryRepository))
+                {
+                    return true;
+                }
+            }
+                return false;
+        }
+
+
+        
+    }
+}
